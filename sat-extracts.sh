046f2534ec50024a90211ca1de4fa2673b468351
George Hunt <georgejhunt@gmail.com>
2019-05-08 19:19:59 -0700

    without these changes, this script delete all the regions I had already created.

--- sat-extracts.sh
+++ sat-extracts.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 readonly CSV_FILE=${CSV_FILE:-"extracts.csv"}
 readonly PLANET_MBTILES=${PLANET_MBTILES:-"planet.mbtiles"}
-readonly SAT_URL=${SAT_URL:-"tms:https://tiles.maps.eox.at/wmts?layer=s2cloudless-2018_3857&style=default&tilematrixset=g&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}"}
+readonly SAT_URL=${SAT_URL:-"tms:https://tiles.maps.eox.at/wmts?layer=s2cloudless-2018_3857&style=default&tilematrixset=g&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={-y}"}
 readonly EXTRACT_DIR=$(dirname "$PLANET_MBTILES")
 readonly PATCH_ZOOM=7  #${BASE_ZOOM:-"5"}
 readonly PATCH_SRC=$EXTRACT_DIR/${PLANET_BASE:-"satellite_z0-z${PATCH_ZOOM}.mbtiles"}
@@ -21,8 +21,8 @@ function main() {
       python -u create_extracts.py zoom-level "$SAT_URL" \
         --max-zoom=${PATCH_ZOOM} --target-dir="$EXTRACT_DIR"
     fi
-    python -u create_extracts.py bbox "$PLANET_MBTILES" "$CSV_FILE" \
-        --patch-from="$PATCH_SRC" --target-dir="$EXTRACT_DIR" $upload_flag
+    #python -u create_extracts.py bbox "$PLANET_MBTILES" "$CSV_FILE" \
+    #    --patch-from="$PATCH_SRC" --target-dir="$EXTRACT_DIR" $upload_flag
 }
 
 main
