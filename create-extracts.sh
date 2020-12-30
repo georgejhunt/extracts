240b5513ff3dc2455ca11235ecb0a956d4a6a3ce
George Hunt <georgejhunt@gmail.com>
2020-12-30 05:25:27 +0000

    add package-lock.json

--- create-extracts.sh
+++ create-extracts.sh
@@ -2,8 +2,9 @@
 set -o errexit
 set -o pipefail
 
-readonly CSV_FILE=${CSV_FILE:-"extracts.csv"}
-readonly PLANET_MBTILES=${PLANET_MBTILES:-"planet.mbtiles"}
+readonly CSV_FILE=${CSV_FILE:-"./data/iiab.csv"}
+# https://ia802807.us.archive.org/21/items/osm-vector-mbtiles/2020-10-planet-14.mbtiles
+readonly PLANET_MBTILES=${PLANET_MBTILES:-"./data/2020-10-planet-14.mbtiles"}
 readonly EXTRACT_DIR=$(dirname "$PLANET_MBTILES")
 readonly PATCH_ZOOM=${BASE_ZOOM:-"9"}
 readonly PATCH_SRC=$EXTRACT_DIR/${PLANET_BASE:-"planet_z0-z${PATCH_ZOOM}.mbtiles"}
